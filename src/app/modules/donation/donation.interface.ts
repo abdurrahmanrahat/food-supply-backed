0560cc98c7181f807b47f2a44fbaaa714bda8de4
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-06-02 20:13:45 +0600

    donation interface

---
+++ src/app/modules/donation/donation.interface.ts
@@ -0,0 +1,10 @@
+export type TDonation = {
+  userName: string;
+  userEmail: string;
+  supplyImg: string;
+  supplyTitle: string;
+  supplyCategory: string;
+  supplyQuantity: string;
+  supplyDesc: string;
+  newSupplyQuantity: string;
+};
