2d04b283fad1792798902b5794698bca87d10c2d
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-06-02 17:41:59 +0600

    validation

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,15 +2,11 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import { sendResponse } from '../../utils/sendResponse';
 import { UserServices } from './user.service';
-import { userValidationSchema } from './user.validation';
 
 const createUser = catchAsync(async (req, res) => {
   const user = req.body;
 
-  // data validation using zod.
-  const validateUser = userValidationSchema.parse(user);
-
-  const result = await UserServices.createUserInfoDb(validateUser);
+  const result = await UserServices.createUserInfoDb(user);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
